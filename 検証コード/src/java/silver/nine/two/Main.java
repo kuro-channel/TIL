514e01ce0bdd0d57edc0e7b8b14da4d2472af95b
kuro-channel <takumikuroiwa@gmail.com>
2020-04-25 14:12:18 +0900

    第9章 第2問 compareメソッド実装

---
+++ 検証コード/src/java/silver/nine/two/Main.java
@@ -0,0 +1,25 @@
+package java.silver.nine.two;
+
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    Sample[] samples = {
+                new Sample(2, "B"),
+                new Sample(3, "C"),
+                new Sample(1, "A")
+        };
+
+        List<Sample> list = new ArrayList<Sample>(Arrays.asList(samples));
+        list.sort(new SampleComparator());
+
+        for(Sample s: list){
+            System.out.println(s.getName());
+        }
+
+    }
+}
