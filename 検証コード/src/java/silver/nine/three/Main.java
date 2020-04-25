7074a63d92c4929d631fec51c43ae7176e06ed76
kuro-channel <takumikuroiwa@gmail.com>
2020-04-25 14:49:06 +0900

    第9章 第2問 compareメソッド実装

---
+++ 検証コード/src/java/silver/nine/three/Main.java
@@ -0,0 +1,27 @@
+package java.silver.nine.three;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // 「3,2,1」と表示したい
+        List<Integer> list1 = Arrays.asList(new Integer[]{1,2,3});
+        List<Integer> list2 = Arrays.asList(new Integer[]{1,2,3});
+
+        //　回答1
+        list1.sort((a,b) -> a.compareTo(b));
+        //　回答2
+        list2.sort((a,b) -> -a.compareTo(b));
+
+        for(Integer num: list1){
+            System.out.println(num);
+        }
+
+        for(Integer num: list2){
+            System.out.println(num);
+        }
+
+    }
+}
