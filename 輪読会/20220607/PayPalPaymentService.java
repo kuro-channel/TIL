c9eeadb3e8e174a2b99ab4ff59c25090ed00f41c
t.kuroiwa <t.kuroiwa.0216@gmail.com>
2022-06-07 23:28:33 +0900

    Create PayPalPaymentService.java

---
+++ 輪読会/20220607/PayPalPaymentService.java
@@ -0,0 +1,11 @@
+/**
+ * PayPal決済用
+ */
+public class PayPalPaymentService implements PaymentInterface{
+
+  @Override
+  public int payment(PaymentModel model) {
+    // TODO Auto-generated method stub
+    return 0;
+  }
+}
