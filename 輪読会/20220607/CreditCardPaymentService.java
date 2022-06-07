d30386c2d3349fb8dfbe829b3526486ad10dc42f
t.kuroiwa <t.kuroiwa.0216@gmail.com>
2022-06-07 23:26:23 +0900

    Create CreditCardPaymentService.java

---
+++ 輪読会/20220607/CreditCardPaymentService.java
@@ -0,0 +1,11 @@
+/**
+ * CreditCard決済用
+ */
+public class CreditCardPaymentService implements PaymentInterface{
+
+  @Override
+  public int payment(PaymentModel model) {
+    // TODO Auto-generated method stub
+    return 0;
+  }
+}
